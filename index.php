4562ede57ece253cec1e7de655970046365a781d
Damianves <99033554@mydavinci.nl>
2016-02-04 14:27:59 +0100

    homepage
    test

---
+++ index.php
@@ -0,0 +1,68 @@
+<html>	
+<head>
+	<title>homepage</title>
+	<link rel="stylesheet" type="text/css" href="exm.css">
+	<meta charset="UTF-8">
+	
+</head>
+<body>
+<ul>
+  <li><a class="active" href="http://www.home.damianvanes.dvc-icta.nl/">Home</a></li>
+  <li><a href="#news">nog niks</a></li>
+  <div class="dropdown">
+    <a href="#" class="dropbtn">Site</a>
+    <div class="dropdown-content">
+      <a href="http://damianvanes.dvc-icta.nl/">Goede...</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div>
+</ul>
+	
+	<div class="me">
+	<img src="#" class="me">
+	<h1> Over mij</h1>
+			<p> Hoi ik ben damian van es en ik ben 16 jaar oud. Op deze site kan dingen vinden over mij en welke opdrachten ik heb gemaakt tijdens mijn opleiding
+			gebruik het menu om te browsen op deze site.of bekijken de plaatjes met tekst hieronder om te weten te komen wat ik heb gemaakt. Klik op de link onder de afbeelding en de tekst om de sites te bekijken.
+			</p>
+		</div>
+	<div class="alles">
+		
+		
+		<div class="goedemorgen">
+			<img src="goedemorgen.png">
+			<p><span class="good">Dit is een opdracht genaamd Goedmorgen <span>Deze opdracht bestaat uit de volgende talen: Html - css - php - javascript.</span></span>
+			<p><span class="wat">Wat kan deze site?<span> Deze site veranderd per deel van de dag. Deze website bevat muziek</span></span></p>
+			
+				<h2><a href="http://damianvanes.dvc-icta.nl/ "TARGET="_blank">bekijk de opdracht</a> </h2>
+			
+		</div>
+		<div class="opdracht1">
+			<img src="madlibs.png">
+			
+			
+				<h2><a href="#">bekijk de opdracht</a> </h2>
+			
+		</div>
+		
+		<div class="opdracht2">
+			<img src="absolutely.png">
+			
+			<h2><a href="#">bekijk de opdracht</a> </h2>
+			
+		</div>
+		<div class="footer"><h4>Made By Damian</h4></div>
+	</div>
+	<div class="footer2">
+		<h4>Made By Damian</h4>
+	</div>
+	
+	
+
+</body>
+<!--#717582;
+margin-left:5%;
+margin-right:1%;
+margin-top:20px;
+border:5px #717582 solid;-->
+</html>	
